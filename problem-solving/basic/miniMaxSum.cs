f62c828a0bc2b52355b42bce47a8bf18c9927b73
Erick Sales <ericksalec@gmail.com>
2022-05-29 15:53:01 -0300

    miniMaxSum

---
+++ problem-solving/basic/miniMaxSum.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+        int length = arr.Count;
+        long min = arr.Take(length - 1).Sum(item => (long)item);
+        long max = arr.Skip(1).Sum(item => (long)item);
+
+        Console.WriteLine($"{min} {max}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
